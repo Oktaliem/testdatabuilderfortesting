4aa747c1b3ec3922790170550211c48e3c6dee48
Okta Liem <surya_diharja@yahoo.co.id>
2020-09-17 22:30:35 +0800

    simplify web scraping test

--- src/main/java/com/oktaliem/builder/WebScraperBuilder.java
+++ src/main/java/com/oktaliem/builder/WebScraperBuilder.java
@@ -10,41 +10,57 @@ import java.io.IOException;
 import static org.testng.Assert.fail;
 
 public class WebScraperBuilder {
+    private final Document doc;
+    private final Elements links;
 
-    public void getDataWithJsoup(String urlStr) {
+    public WebScraperBuilder(String url) {
+        doc = setUpJsoupConnection(url);
+        links = doc.select("a[href]");
+    }
+
+    public void getTitleAndBody() {
+        String title = doc.title();
+        System.out.println("Title: " + title);
+        String text = doc.body().text();
+        System.out.println("Text: " + text);
+    }
+
+    public void getHrefLink() {
+        for (Element link : links) {
+            String linkHref = link.attr("href");
+            System.out.println("linkHref: " + linkHref);
+        }
+    }
+
+    public void getLinkText() {
+        for (Element link : links) {
+            String linkText = link.text();
+            System.out.println("linkText: " + linkText);
+        }
+    }
+
+    public void getOuterHtml() {
+        for (Element link : links) {
+            String linkOuterHtml = link.outerHtml();
+            System.out.println("linkOuterHtml: " + linkOuterHtml);
+        }
+    }
+
+    public void getInnerHtml() {
+        for (Element link : links) {
+            String linkInnerHtml = link.html();
+            System.out.println("linkInnerHtml: " + linkInnerHtml);
+        }
+    }
+
+    private Document setUpJsoupConnection(String urlString) {
         Document doc = null;
         try {
-            doc = Jsoup.connect(urlStr)
-                    .timeout(10 * 1000)
-                    .get();
+            doc = Jsoup.connect(urlString).timeout(10 * 1000).get();
         } catch (IOException e) {
             System.out.println("Error found --> " + e);
             fail();
         }
-        if (doc != null) {
-            String title = doc.title();
-            System.out.println("Title: " + title);
-            String text = doc.body().text();
-            System.out.println("Text: " + text);
-            Elements links = doc.select("a[href]");
-
-            for (Element link : links) {
-                System.out.println("=============================================================================");
-                String linkHref = link.attr("href");
-                System.out.println("linkHref: " + linkHref);
-                String linkText = link.text();
-                System.out.println("linkText: " + linkText);
-                String linkOuterHtml = link.outerHtml();
-                System.out.println("linkOuterHtml: " + linkOuterHtml);
-                String linkInnerHtml = link.html();
-                System.out.println("linkInnerHtml: " + linkInnerHtml);
-            }
-            System.out.println("Links by index: " + links.get(11));
-            System.out.println("Links by Text Random: " + links.get(11).getElementsMatchingOwnText("Random"));
-            System.out.println("Links size: " + links.size());
-            System.out.println("Links by Tag: " + doc.getElementsByTag("span"));
-            System.out.println("Link Name by Tag: " + doc.getElementsByTag("span").get(11));
-        }
-
+        return doc;
     }
 }
---
+++ src/test/java/com/oktaliem/unittest/WebScrapingBuilderTest.java
@@ -0,0 +1,34 @@
+package com.oktaliem.unittest;
+
+import com.oktaliem.builder.WebScraperBuilder;
+import org.junit.Test;
+
+public class WebScrapingBuilderTest {
+    public static final String url ="https://www.randomlists.com/";
+    WebScraperBuilder scrappers = new WebScraperBuilder(url);
+
+    @Test
+    public void getTitleAndBody() {
+        scrappers.getTitleAndBody();
+    }
+
+    @Test
+    public void getHrefLink() {
+        scrappers.getHrefLink();
+    }
+
+    @Test
+    public void getLinkText() {
+        scrappers.getLinkText();
+    }
+
+    @Test
+    public void getOuterHtml() {
+        scrappers.getOuterHtml();
+    }
+
+    @Test
+    public void getInnerHtml() {
+        scrappers.getInnerHtml();
+    }
+}
